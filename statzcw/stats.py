a70a2fa768227c33b5cbc9d9a6eef0ead4feba29
jwheller0013 <jwheller0013@yahoo.com>
2025-03-24 16:31:06 -0400

    working on stats.py

--- statzcw/stats.py
+++ statzcw/stats.py
@@ -1,27 +1,44 @@
-
+import math
 from typing import List
 
 def zcount(data: List[float]) -> float :
     pass
+    return sum(data)
 
 def zmean(data: List[float]) -> float :
     pass
+    return sum(data)/len(data)
 
 def zmode(data: List[float]) -> float :
     pass
+    mode = 0
+    for i in data:
+        if data.count(i) > mode:
+            mode = i
+    return mode
 
 def zmedian(data: List[float]) -> float :
     pass
+    middle = len(data)//2
+    return data(middle)
 
 def zvariance(data: List[float]) -> float :
     pass
+    n = data.count() - 1
+    mean = sum(data)/n
+    deviations = []
+    for xi in data:
+        deviations.append(math.sqrt(abs(mean - xi)))
+    return sum(deviations)/n
 	
 def zstddev(data: List[float]) -> float :
     # sqrt of variance
     pass
+    return math.sqrt(zvariance(data))
 
 def zstderr(data: List[float]) -> float :
     pass
+    return zstddev(data)/((zcount(data)) ** 0.5)
 
 def cov(a, b):
     pass
